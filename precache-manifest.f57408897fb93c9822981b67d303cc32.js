da33ddbb36b005dfa4e429cd018a62b1dd3592eb
BinongKBey <binongkbey1@gmail.com>
2020-04-25 10:01:47 +0530

    Updates

---
+++ precache-manifest.f57408897fb93c9822981b67d303cc32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678881f6c6fc53ccdbca850bfa63366e",
+    "url": "/react-builds/index.html"
+  },
+  {
+    "revision": "78640219057f14bfa661",
+    "url": "/react-builds/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ab82a556baacc5cec2d3",
+    "url": "/react-builds/static/js/2.ef84220a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-builds/static/js/2.ef84220a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78640219057f14bfa661",
+    "url": "/react-builds/static/js/main.145e227f.chunk.js"
+  },
+  {
+    "revision": "65e3183bf94989b68699",
+    "url": "/react-builds/static/js/runtime-main.6940eb9d.js"
+  }
+]);
